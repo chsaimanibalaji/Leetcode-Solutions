af98414f4cf91e72989904d81f5f2c4fae7ea108
chandramahanthi sai mani balaji <saimanibalaji24@ngit.edu.in>
2026-07-10 12:26:53 +0530

    Time: 0 ms (100%), Space: 48.3 MB (55.06%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i=0,j=s.length-1;
-        while(i<j){
+        int i=0;
+        while(i<s.length/2){
             char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            s[i]=s[s.length-i-1];
+            s[s.length-i-1]=temp;
             i++;
-            j--;
         }
     }
 }
