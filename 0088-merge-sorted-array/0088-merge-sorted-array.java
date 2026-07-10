066e77aee18c875c61a1204eaaa657a555e2601f
chandramahanthi sai mani balaji <saimanibalaji24@ngit.edu.in>
2026-07-10 12:18:05 +0530

    Time: 4 ms (17.19%), Space: 44 MB (15.55%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int x=0;
+        for(int i=m;i<nums1.length;i++){
+            nums1[i]=nums2[x];
+            x++;
+        }
+        Arrays.sort(nums1);
+    }
+}
