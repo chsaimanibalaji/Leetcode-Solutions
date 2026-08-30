a8c29da4940a6cc8db403125f1846ec109b1b4de
chandramahanthi sai mani balaji <saimanibalaji24@ngit.edu.in>
2026-08-30 12:03:36 +0530

    Time: 3 ms (86.08%), Space: 43.1 MB (85.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,60 @@
+/*import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        int i=0;
+        int flag=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+                flag++;
+            }
+            if(flag==0){
+                return false;
+            }
+            char x=stack.peek();
+            if(x=='(' && s.charAt(i)==')'){
+                char p=stack.pop();
+                flag--;
+            }
+            if(x=='{' && s.charAt(i)=='}'){
+                char p=stack.pop();
+                flag--;
+            }
+            if(x=='[' && s.charAt(i)==']'){
+                char p=stack.pop();
+                flag--;
+            }
+            i++;
+        }
+        if(stack.empty()){
+            return true;
+        }
+        return false;
+    }
+}*/
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        
+        Deque<Character> stack = new ArrayDeque<>();   
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            // 1. If it's an opening bracket, push it
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } 
-            // 2. If it's a closing bracket
             else {
-                // If stack is empty, there's no matching opening bracket
                 if (stack.isEmpty()) {
                     return false;
                 }
-                
-                // Check if top of stack matches the current closing bracket
                 char top = stack.peek();
                 if ((c == ')' && top == '(') ||
                     (c == '}' && top == '{') ||
                     (c == ']' && top == '[')) {
-                    stack.pop(); // Valid match!
+                    stack.pop(); 
                 } else {
-                    return false; // Mismatched types (e.g., top is '(', but c is ']')
+                    return false;
                 }
             }
         }
-        
-        // If stack is empty, all brackets were properly matched
         return stack.isEmpty();
     }
 }
