7117b43818e89241a6074f48e6ec0e7009bfccbf
chandramahanthi sai mani balaji <saimanibalaji24@ngit.edu.in>
2026-09-06 12:57:47 +0530

    Time: 0 ms (100%), Space: 44.4 MB (92.28%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        int mid=(low+high)/2;
+        if(target>nums[high]){
+            return high+1;
+        }
+        if(target<nums[low]){
+            return 0;
+        }
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
