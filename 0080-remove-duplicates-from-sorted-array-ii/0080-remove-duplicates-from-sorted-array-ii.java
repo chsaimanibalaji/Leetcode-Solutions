bfa1f987df68d49092e743055ec48872ff55bbfe
chandramahanthi sai mani balaji <saimanibalaji24@ngit.edu.in>
2026-09-20 19:19:28 +0530

    Time: 6 ms (3.9%), Space: 49.2 MB (15.22%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+    int buffer=0,dup=0;
+    int i=0,j=1;
+    if(nums.length==1){
+        return 1;
+    }
+    if(nums.length==2){
+        return 2;
+    }
+    while(i<nums.length && j<nums.length){
+        if(i<j && nums[i]==nums[j]){
+            buffer++;
+        }
+        if(nums[j]!=nums[i]){
+            buffer=0;
+            i=j;
+            j++;
+            continue;
+        }
+        if(buffer>1){
+            nums[j]=Integer.MAX_VALUE;
+            dup++;
+        }
+        j++;
+    }
+    Arrays.sort(nums);
+    return nums.length-dup;
+    }
+}
